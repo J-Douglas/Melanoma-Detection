eede31b1261ce399081a9db0e223c1e0a561be14
Jack Douglas <jackdouglas145@gmail.com>
2020-08-01 23:56:50 -0400

    classifcation model setup

--- disease-classification/skin_diesease_classification.py
+++ disease-classification/skin_diesease_classification.py
@@ -6,11 +6,20 @@ from keras.models import Sequential
 from keras.layers import *
 from keras.utils import to_categorical
 
-df = pd.read_csv('../datasets/HAM/HAM10000_metadata.csv')
+df_labels = pd.read_csv('../datasets/HAM/HAM10000_metadata.csv')
 
-train_img = pd.read_csv('../datasets/HAM/')
-train_labels
+train_img = df.iloc[]
+train_labels =
 test_img
 test_labels
 
 model = Sequential()
+model.add(Conv2D(32,(5,5), activation='relu', input_shape=(28,28,1)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (5, 5), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(6, activation='softmax'))
+
