bd8abc31792a0b3ea264c957cde228ade64f4171
Jack Douglas <jackdouglas145@gmail.com>
2020-08-01 23:41:14 -0400

    classification df setup

--- disease-classification/skin_diesease_classification.py
+++ disease-classification/skin_diesease_classification.py
@@ -1,3 +1,16 @@
 import tensorflow as tf
+import keras
 import pandas as pd
 import numpy as np
+from keras.models import Sequential
+from keras.layers import *
+from keras.utils import to_categorical
+
+df = pd.read_csv('../datasets/HAM/HAM10000_metadata.csv')
+
+train_img = pd.read_csv('../datasets/HAM/')
+train_labels
+test_img
+test_labels
+
+model = Sequential()
