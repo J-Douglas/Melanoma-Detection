4993c0a7d14eb037a2ec5be5daacace7f211ad27
Jack Douglas <jackdouglas145@gmail.com>
2020-08-19 14:29:22 -0400

    reorganization/classifcation v1

--- disease-classification/skin_disease_classification.py
+++ disease-classification/skin_disease_classification.py
@@ -16,60 +16,85 @@ def append_ext(fn):
 
 df['image_id'] = df['image_id'].apply(append_ext)
 
-base_dir = '../datasets/HAM/HAM10016_images/'
+### Organizing HAM Dataset
+
+# base_dir = '../datasets/HAM/'
  
-class_buckets = [0] * 7
+# class_buckets = [0] * 7
 
-class_dict = {
-    "akiec": 0,
-    "bcc": 1,
-    "bkl": 2,
-    "df": 3,
-    "mel": 4,
-    "nv": 5,
-    "vasc": 6
-}
+# class_dict = {
+#     "akiec": 0,
+#     "bcc": 1,
+#     "bkl": 2,
+#     "df": 3,
+#     "mel": 4,
+#     "nv": 5,
+#     "vasc": 6
+# }
 
-for elm in df['dx']:
-    if elm in class_dict:
-        class_buckets[class_dict[elm]] += 1
+# for elm in df['dx']:
+#     if elm in class_dict:
+#         class_buckets[class_dict[elm]] += 1
 
-for i in range(7):
-    print(class_buckets[i])
+# for i in range(7):
+#     print(class_buckets[i])
 
-for elm in df['image_id']:
-    disease_class = df[df['image_id']==elm]['dx'].iloc[0]
-    print(base_dir)
-    print(elm)
-    print(disease_class)
-    os.rename(base_dir + elm,'../datasets/HAM/' + disease_class + '/' + elm)
+# for elm in df['image_id']:
+#     disease_class = df[df['image_id']==elm]['dx'].iloc[0]
+#     print(base_dir)
+#     print(elm)
+#     print(disease_class)
+#     os.rename(base_dir + elm,'../datasets/HAM/' + disease_class + '/' + elm)
 
-# model = Sequential()
-# model.add(Conv2D(32,(101, 101), activation='relu', input_shape=(600,450,3)))
-# # model.add(MaxPooling2D((2, 2)))
-# model.add(Conv2D(64, (51, 51), activation='relu'))
-# model.add(MaxPooling2D((2, 2)))
-# model.add(Conv2D(64, (7, 7), activation='relu'))
-# model.add(Conv2D(64, (5, 5), activation='relu'))
-# model.add(Conv2D(64, (3, 3), activation='relu'))
-# # model.add(MaxPooling2D((2, 2)))
-# model.add(Flatten())
-# model.add(Dense(7, activation='softmax'))
+model = Sequential()
+model.add(Conv2D(8,(7, 7), activation='relu', input_shape=(600,450,3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(16,(7, 7), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(16, (5, 5), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(32, (5, 5), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(16, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(8, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(7, activation='softmax'))
 
-# model.compile(
-#   optimizer='adam', 
-#   loss='categorical_crossentropy', 
-#   metrics=['accuracy']
-# )
+model.compile(
+  optimizer='adam', 
+  loss='categorical_crossentropy', 
+  metrics=['accuracy']
+)
 
-# model.summary()
+model.summary()
 
-# epoch_count = 4
-# batch_size = 120
+epoch_count = 4
+batch_size = 16
 
-# ### Image Generators (train and testing data)
-# train_datagen = ImageDataGenerator(rescale=1/255)
-# test_datagen = ImageDataGenerator(rescale=1/255)
+### Image Generators (train and testing data)
+train_datagen = ImageDataGenerator(
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    fill_mode="nearest",
+    horizontal_flip=True,
+    vertical_flip=True,
+    rescale=1./255)
+validation_datagen = ImageDataGenerator(
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    fill_mode="nearest",
+    horizontal_flip=True,
+    vertical_flip=True,
+    rescale=1./255)
+test_datagen = ImageDataGenerator(
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    fill_mode="nearest",
+    horizontal_flip=True,
+    vertical_flip=True,
+    rescale=1./255)
 
 # split = int(df.shape[0]*0.8)
 
@@ -97,28 +122,40 @@ for elm in df['image_id']:
 #     target_size=(600,450)
 # )
 
-# # train_generator = train_datagen.flow_from_directory(
-# # 		'../datasets/train',
-# #         target_size=(600, 450), 
-# #         batch_size=batch_size,
-# #         classes = ['akiec','bcc','bkl','df','mel','nv','vasc'],
-# #         class_mode='categorical')
+train_generator = train_datagen.flow_from_directory(
+        batch_size=batch_size,
+		directory='../datasets/HAM/train',
+        target_size=(600, 450), 
+        classes = ['akiec','bcc','bkl','df','mel','nv','vasc'],
+        class_mode='categorical')
 
-# # test_generator = test_datagen.flow_from_directory(
-# # 		'../datasets/test',
-# #         target_size=(600, 450), 
-# #         batch_size=batch_size,
-# #         classes = ['akiec','bcc','bkl','df','mel','nv','vasc'],
-# #         class_mode='categorical')
+validation_generator = validation_datagen.flow_from_directory(
+        batch_size=batch_size,
+        directory='../datasets/HAM/validation',
+        target_size=(600, 450), 
+        classes = ['akiec','bcc','bkl','df','mel','nv','vasc'],
+        class_mode='categorical')
 
-# model.fit(
-# 	  train_generator, 
-#     steps_per_epoch=int(split/batch_size),  
-#     epochs=epoch_count,
-#     verbose=1)
+test_generator = test_datagen.flow_from_directory(
+		batch_size=batch_size,
+        directory='../datasets/HAM/test',
+        target_size=(600, 450), 
+        classes = ['akiec','bcc','bkl','df','mel','nv','vasc'],
+        class_mode='categorical')
 
-# # Saving the model
-# model.save_weights('skin_disease_classification.h5'.format(epoch_count))
+print("Got HERE")
+
+model.fit_generator(
+	train_generator,
+    steps_per_epoch=10,
+    epochs=epoch_count,
+    verbose=1,  
+    validation_data=validation_generator,)
+
+print("HERE yet?")
+
+# Saving the model
+model.save_weights('classify_v2.h5'.format(epoch_count))
 
 # test_pred = pd.DataFrame(model.predict(test_datagen))
 # test_pred = pd.DataFrame(test_pred.idxmax(axis = 1))
