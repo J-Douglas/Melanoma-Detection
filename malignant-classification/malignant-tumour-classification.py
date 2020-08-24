f6bfa93e2454d46670cc0c7c36398d6b33db3b15
Jack Douglas <jackdouglas145@gmail.com>
2020-08-24 19:19:23 -0400

    resnet50

--- malignant-classification/malignant-tumour-classification.py
+++ malignant-classification/malignant-tumour-classification.py
@@ -5,6 +5,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import *
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.applications import ResNet50
 
 ### Datasets
 df_train = pd.read_csv('../datasets/ISIC 2020/train.csv')
@@ -18,17 +19,88 @@ def append_ext(fn):
 df_train['image_name'] = df_train['image_name'].apply(append_ext)
 df_test['image_name'] = df_test['image_name'].apply(append_ext)
 
+### Image Generators (train and testing data)
+
+train_datagen = ImageDataGenerator(
+    width_shift_range=0.3,
+    height_shift_range=0.3,
+    fill_mode="nearest",
+    horizontal_flip=True,
+    vertical_flip=True,
+    rescale=1./255)
+
+validation_datagen = ImageDataGenerator(
+    width_shift_range=0.3,
+    height_shift_range=0.3,
+    fill_mode="nearest",
+    horizontal_flip=True,
+    vertical_flip=True,
+    rescale=1./255)
+
+test_datagen = ImageDataGenerator(
+    width_shift_range=0.3,
+    height_shift_range=0.3,
+    fill_mode="nearest",
+    horizontal_flip=True,
+    vertical_flip=True,
+    rescale=1./255)
+
+# train_generator = train_datagen.flow_from_directory(
+#         batch_size=batch_size,
+# 		directory='../datasets/HAM/train',
+#         target_size=(600, 450), 
+#         classes = ['akiec','bcc','bkl','df','mel','nv','vasc'],
+#         class_mode='categorical')
+
+# validation_generator = validation_datagen.flow_from_directory(
+#         batch_size=batch_size,
+#         directory='../datasets/HAM/validation',
+#         target_size=(600, 450), 
+#         classes = ['akiec','bcc','bkl','df','mel','nv','vasc'],
+#         class_mode='categorical')
+
+# test_generator = test_datagen.flow_from_directory(
+# 		batch_size=batch_size,
+#         directory='../datasets/HAM/test',
+#         target_size=(600, 450), 
+#         classes = ['akiec','bcc','bkl','df','mel','nv','vasc'],
+#         class_mode='categorical')
+
 ### Custom Model
 
 model = Sequential()
 model.add(Conv2D(32,(5,5), activation='relu', input_shape=(6000,4000,3)))
 model.add(Conv2D(64,(5,5), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
 model.add(Conv2D(64,(5,5), activation='relu'))
 model.add(Conv2D(64,(3,3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64,(3,3), activation='relu'))
 model.add(Conv2D(32,(3,3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
 model.add(Conv2D(16,(3,3), activation='relu'))
 model.add(Conv2D(8,(3,3), activation='relu'))
 model.add(Dense(2, activation='softmax'))
 
 ### Pre-trained Model w/ Transfer Learning
+
+model = tf.keras.applications.ResNet51(include_top=False,weights='imagenet')
+
+# transfer learning
+for i in model.layers:
+  i.trainable = False
+
+global_avg = tf.keras.layers.GlobalAveragePooling2D()(model.output)
+drop_out = tf.keras.layers.Dropout(0.4)(global_avg)
+out = tf.keras.layers.Dense(2,activation='sigmoid')(drop_out)
+resnet = tf.keras.Model(inputs=[model.input],outputs=[out])
+
+resnet.compile(optimizer=tf.keras.optimizers.Adam(),loss="binary_crossentropy",metrics=["accuracy"])
+
+history = nasnet.fit_generator(
+    # train_data_gen,
+    # epochs=epochs,
+    validation_data=val_data_gen
+)
+
 
